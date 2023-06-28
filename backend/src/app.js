62329b57cd28c77a9a5321ac93cbc6d1d5bc6c06
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-06-28 14:56:14 -0300

    first commit e cria model da products com findall/findbyid

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
