046f9bd4e06d65aa1d288a0cc3e4adca1eb04443
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-06-29 14:19:52 -0300

    implementa middlewares e resolve req 4

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const products = require('./routes/products');
 const sales = require('./routes/sales');
+const { validateName } = require('./middlewares/validateFields');
 
 const app = express();
 app.use(express.json());
-app.use('/products', products);
+app.use('/products', validateName, products);
 app.use('/sales', sales);
 
 // não remova esse endpoint, é para o avaliador funcionar
